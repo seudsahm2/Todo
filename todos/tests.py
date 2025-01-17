0994a00970d0409af570c8972fae1df51aa003cb
seud <seudsahm1@gmail.com>
2025-01-17 22:55:17 +0300

    test implemented for the model and customizeed admin panel, added the model to the admin panel

--- todos/tests.py
+++ todos/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
-
+from .models import Todo
 # Create your tests here.
+
+
+class TodoModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.todo = Todo.objects.create(
+            title="test title",
+            body="test body"
+        )
+
+    def test_model_content(self):
+        self.assertEqual(self.todo.title, "test title")
+        self.assertEqual(self.todo.body, "test body")
+        self.assertEqual(str(self.todo), "test title")
