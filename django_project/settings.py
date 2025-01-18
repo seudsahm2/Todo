415b1b725eb6a618de0cdb98f64b6f55ee44aec8
seud <seudsahm1@gmail.com>
2025-01-18 09:04:19 +0300

    todo api and test implemented

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,8 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+
     'todos.apps.TodosConfig',
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- todos/tests.py
+++ todos/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
 from .models import Todo
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 # Create your tests here.
 
 
@@ -15,3 +18,16 @@ class TodoModelTest(TestCase):
         self.assertEqual(self.todo.title, "test title")
         self.assertEqual(self.todo.body, "test body")
         self.assertEqual(str(self.todo), "test title")
+
+    def test_api_list_view(self):
+        response = self.client.get(reverse("todo_list"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Todo.objects.count(), 1)
+        self.assertContains(response, self.todo)
+
+    def test_api_detail_view(self):
+        response = self.client.get(
+            reverse("todo_detail", kwargs={"pk": self.todo.id}), format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Todo.objects.count(), 1)
+        self.assertContains(response, "test title")
