f47439a7e05e54cc69f864be1727f16f307adfe7
seud <seudsahm1@gmail.com>
2025-01-18 09:41:14 +0300

    CSRF TRUSTD ORIGINS modified

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&dv5**@)k&eqr2_fj+bks+**!v64c3i8#&k^(00qzbs#a4c2o_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".herokuapp.com","localhost","127.0.0.1"]
+ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -145,4 +145,4 @@ CORS_ALLOWED_ORIGINS = (
     "http://localhost:8000",
 )
 
-CSRF_TRUSTED_ORIGINS = ["localhost:3000"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost:3000"]
